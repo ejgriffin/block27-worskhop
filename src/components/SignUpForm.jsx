93b4914a39b7529632cc9617e67ff607cf0c865e
Eric Griffin <ejgriffin13@gmail.com>
2023-11-14 19:41:30 -0500

    finished Authenticate Component?

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 
-export default function SignUpForm() {
+export default function SignUpForm({ setToken }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
   async function handleSubmit(event) {
     event.preventDefault();
-
     try {
       const response = await fetch(
         "https://fsa-jwt-practice.herokuapp.com/signup",
@@ -19,14 +18,15 @@ export default function SignUpForm() {
           }),
         }
       );
+
       const result = await response.json();
-      console.log(result);
+      setToken(result.token);
     } catch (err) {
       setError(error.message);
     }
   }
   return (
-    <>
+    <div>
       <h2>Sign Up!</h2>
       {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
@@ -40,12 +40,13 @@ export default function SignUpForm() {
         <label>
           Password:{" "}
           <input
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
         <button>Submit</button>
       </form>
-    </>
+    </div>
   );
 }
